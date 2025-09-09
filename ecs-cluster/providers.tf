cd222fd373d8e6da85be11432d34e9870a77c7c5
JoseH <keeper_sdsu@yahoo.com>
2025-09-09 15:44:10 -0700

    changes

--- ecs-cluster/providers.tf
+++ ecs-cluster/providers.tf
@@ -10,5 +10,5 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
--- eks-cluster-module/versions.tf
+++ eks-cluster-module/versions.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 6.0"
     }
   }
 }
@@ -14,5 +10,4 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"
 }
